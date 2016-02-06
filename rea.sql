ffe59f55b65f9d399780ef1c97bd7831f015595c
Дмитрий Маскаев <maskaevda@gmail.com>
2016-02-07 01:17:11 +0300

    Добавление дампа Структуры БД.

---
+++ rea.sql
@@ -0,0 +1,94 @@
+# ************************************************************
+# Sequel Pro SQL dump
+# Версия 4500
+#
+# http://www.sequelpro.com/
+# https://github.com/sequelpro/sequelpro
+#
+# Адрес: localhost (MySQL 5.5.42)
+# Схема: rea
+# Время создания: 2016-02-06 22:14:24 +0000
+# ************************************************************
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+# Дамп таблицы buyers
+# ------------------------------------------------------------
+
+CREATE TABLE `buyers` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_users` int(11) DEFAULT NULL,
+  `id_sale` int(11) NOT NULL,
+  `naimen` text NOT NULL,
+  `price` double(9,2) NOT NULL,
+  `bay` tinyint(1) NOT NULL,
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `doc` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+# Дамп таблицы class_user
+# ------------------------------------------------------------
+
+CREATE TABLE `class_user` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `description` varchar(35) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+# Дамп таблицы real
+# ------------------------------------------------------------
+
+CREATE TABLE `real` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL DEFAULT '',
+  `price` double(9,2) NOT NULL,
+  `cover` text,
+  `description` text NOT NULL,
+  `reserve` tinyint(1) NOT NULL,
+  `category` text NOT NULL,
+  `img2` text,
+  `img3` text,
+  `img4` text,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+# Дамп таблицы users
+# ------------------------------------------------------------
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(30) NOT NULL DEFAULT '',
+  `email` varchar(70) DEFAULT NULL,
+  `password` char(40) NOT NULL DEFAULT '',
+  `name` varchar(57) NOT NULL DEFAULT '',
+  `created_at` timestamp NULL DEFAULT NULL,
+  `active` char(1) DEFAULT NULL,
+  `class` char(2) DEFAULT NULL,
+  `phone` varchar(18) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
